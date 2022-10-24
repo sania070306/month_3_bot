ce792b2d62adf2c79f11a205dd5ac330bbcaef15
sania070306 <alsanade.s@gmail.com>
2022-10-24 22:58:08 +1100

    did hw

---
+++ handlers/notifications.py
@@ -0,0 +1,26 @@
+import aioschedule
+from aiogram import types,Dispatcher
+from config import bot
+import asyncio
+
+
+
+async def get_chat_id(message: types.Message):
+    global chat_id
+    chat_id = message.from_user.id
+    await message.answer("OK!")
+
+async def weather():
+    url = 'https://www.gismeteo.ru/weather-yuzhno-kurilsk-4896/'
+    await bot.send_message(chat_id=chat_id, text=url)
+
+
+async def scheduler():
+    aioschedule.every().tuesday.at("7:30").do(weather)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(2)
+
+
+def register_handlers_notifications(dp: Dispatcher):
+    dp.register_message_handler(get_chat_id, lambda word: "Погода" in word.text)
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+import asyncio
+
 from aiogram.utils import executor
 from config import dp
 import logging
-from handlers import client, callback, extra, FsmMentor, admin
+from handlers import client, callback, extra, FsmMentor, admin, notifications
 from database.bot_db import sql_create
 
+
 async def on_startup(_):
+    asyncio.create_task(notifications.scheduler())
     sql_create()
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 FsmMentor.register_handlers_fsm_list_of_mentors(dp)
 admin.register_handlers_admin(dp)
+notifications.register_handlers_notifications(dp)
 
 extra.register_handlers_extra(dp)
 
